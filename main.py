bd39754c9acafffa811844e48c7059a0dc58ace2
roshnigith <90918248+roshnigith@users.noreply.github.com>
2026-02-20 22:39:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-# SQLAlchemy setup (SQLite requires check_same_thread)
+
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
